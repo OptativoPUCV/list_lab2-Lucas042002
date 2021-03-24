26581709382bd3e5c29dc2469f80ef75803a21b6
Replit user <replituser@example.com>
2021-03-24 21:55:43 +0000

    new try

--- list.c
+++ list.c
@@ -100,7 +100,7 @@ void pushCurrent(List * list, const void * data) {
     }
 
     list->current->next = nuebo;
-    int *dato = (int*) list->current->next->data;
+    int *dato = (int*) list->current->data;
     printf ("\ndato = %d\n", *dato);
     nuebo->prev = list->current;
 }
