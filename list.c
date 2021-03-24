5aca003f9d3710c453d2737e37bd5f33b0534e4e
Replit user <replituser@example.com>
2021-03-24 23:10:06 +0000

    new try

--- list.c
+++ list.c
@@ -76,8 +76,12 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, const void * data) {
-    //Node * aux = createNode(data);
-    
+    Node * aux = createNode(data);
+    aux->next = list->head;
+    if(list->head != NULL)
+      list->head->prev = aux;
+    list->head = aux;
+    aux->prev=NULL;
 
     
 }
